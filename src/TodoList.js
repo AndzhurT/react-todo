cfa487af5c3a9a90d033bc35010f3a8cc893df5c
AndzhurT <andjur38@gmail.com>
2023-06-12 12:51:12 -0400

    lesson week 1_2

---
+++ src/TodoList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function TodoList() {
+  return (
+    <ul>
+      <li>list item 1</li>
+      <li>list item 2</li>
+      <li>list item 3</li>
+    </ul>    
+  );
+}
+
+export default TodoList;
