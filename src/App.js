edd2f248e68cdb51c01e4cc774b381482a4dbc04
AndzhurT <andjur38@gmail.com>
2023-06-12 12:46:30 -0400

    lesson week 1_1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
+//to do list
+const todoList = [];
 
 function App() {
+  todoList.push(
+    { id: 1, title: 'Do a react homework' },
+    { id: 2, title: 'Text a friend' },
+    { id: 3, title: 'Go to the gym' }
+  );
+
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo List</h1>
+      <ul>
+        {todoList.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
