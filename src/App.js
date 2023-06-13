9903fac12ed359cf9637ae524cad174f9c0a2021
AndzhurT <andjur38@gmail.com>
2023-06-13 12:27:57 -0400

    fixed app and index file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
 //to do list
-const todoList = [];
+const todoList = [
+  { id: 1, title: 'Do a react homework' },
+  { id: 2, title: 'Text a friend' },
+  { id: 3, title: 'Go to the gym' },
+];
 
-function App() {
-  todoList.push(
-    { id: 1, title: 'Do a react homework' },
-    { id: 2, title: 'Text a friend' },
-    { id: 3, title: 'Go to the gym' }
-  );
-
+function TodoList() {
   return (
-    <div>
-      <h1>Todo List</h1>
-      <ul>
-        {todoList.map((todo) => (
-          <li key={todo.id}>{todo.title}</li>
-        ))}
-      </ul>
-    </div>
+    <ul>
+      {todoList.map((todo) => (
+        <li key={todo.id}>{todo.title}</li>
+      ))}
+    </ul>
   );
 }
 
-export default App;
+export default TodoList;
