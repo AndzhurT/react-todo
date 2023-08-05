e29c437a3f0b2285ab6ae9a2de728705255492a1
AndzhurT <andjur38@gmail.com>
2023-08-04 20:33:22 -0400

    custom hooks/side-effects/fragments

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
-import {useState} from 'react';
 //to do list
 
+function useSemiPersistentState(key) {
+  // Save the var
+  const savedTodoList = JSON.parse(localStorage.getItem(key));
+
+  // Create new state variable named "todoList" with setter "setTodoList" 
+  // and default value pf a savedTodoList or an empty Array []
+  const [todoList, setTodoList] = useState(savedTodoList || []);
+
+  // useEffect
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(todoList))
+  }), [key, todoList];
+  
+  return (
+    [todoList, setTodoList]
+  )
+}
 
 function Main() {
-  // Create new state variable named "todoList" with setter "setTodoList" and default value of an empty Array []
-  const [todoList, setTodoList] = useState([]);
+  // Create new state variable named "todoList" with setter "setTodoList" 
+  // and default value as the savedTodoList from localStorage
+
+  const [todoList, setTodoList] = useSemiPersistentState("savedTodoList");
 
   function addTodo(newTodo) {
     setTodoList([...todoList, newTodo])
   };
+  
 
   return (
-    <div>
+    // empty tags - a short syntax for declaring fragments
+    <>
       <h1> Todo List </h1>
-      <TodoList todoList={todoList} />
       {/* callback handler */}
       <AddTodoForm onAddTodo={addTodo}/>
+      {/* adds list items */}
+      <TodoList todoList={todoList} />
       <p></p>
-    </div>
+    </>
   );
 }
 
